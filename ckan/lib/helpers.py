30fe0ee7ba1afa488b22215491609f065c370b06
Knud Möller <dunken69@gmail.com>
2015-01-05 15:54:31 +0100

    replaced http with https for gravatar and googleapis

--- ckan/lib/helpers.py
+++ ckan/lib/helpers.py
@@ -492,7 +492,7 @@ def gravatar(email_hash, size=100, default=None):
         # treat the default as a url
         default = urllib.quote(default, safe='')
 
-    return literal('''<img src="http://gravatar.com/avatar/%s?s=%d&amp;d=%s"
+    return literal('''<img src="https://gravatar.com/avatar/%s?s=%d&amp;d=%s"
         class="gravatar" />'''
         % (email_hash, size, default)
         )
