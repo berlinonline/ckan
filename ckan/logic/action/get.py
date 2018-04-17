f25dfb446bab41f897f598d5a2ab4b6d1e4bbc04
Knud Möller <knud@datalysator.com>
2018-04-17 10:40:16 +0200

    ignore groups in list if not authorized

--- ckan/logic/action/get.py
+++ ckan/logic/action/get.py
@@ -439,7 +439,10 @@ def _group_or_org_list(context, data_dict, is_org=False, compute_full=False):
                 if key not in data_dict:
                     data_dict[key] = False
 
-            group_list.append(logic.get_action(action)(context, data_dict))
+            try:
+                group_list.append(logic.get_action(action)(context, data_dict))
+            except logic.NotAuthorized:
+                pass
     else:
         group_list = [getattr(group, ref_group_by) for group in groups]
 
